6ba7dca93da4dbfcf2e58a7e62cc430bc5dc0c10
BuildTools <unconfigured@null.spigotmc.org>
2025-04-13 14:15:18 +0300

    initial commit

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="...">
+      <h1>Welcome to Next.js!</h1>
+      {/* more stuff */}
+    </main>
+  )
+}
